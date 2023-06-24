2709640eb32d344d1a9a441802494b6153b01d64
couscous <87697984+couscous1@users.noreply.github.com>
2023-06-24 05:32:43 +0200

    Add files via upload

---
+++ pressure_at_sealevel.py
@@ -0,0 +1,9 @@
+import weatherapp
+
+p = float (weatherapp.get_measurements()[1]) # pressure
+t = float (weatherapp.get_measurements()[0]) # temperature
+
+def pressure_at_sealevel(height): # ask for height once at runtime
+
+    P0 = p * pow(1 - (0.0065 * height) / (t + (0.0065 * height) + 273.15), -5.257)
+    return P0
