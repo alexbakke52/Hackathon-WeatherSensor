035e6ac2ec583932952f13a3a0c72b4a57de87f7
Alexander Skretting <62384087+alexbakke52@users.noreply.github.com>
2023-06-24 04:59:21 +0200

    Add files via upload
    Added alarm sound effect to precipitation_warning()

--- weatherapp.py
+++ weatherapp.py
@@ -8,28 +8,34 @@ from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg,
 NavigationToolbar2Tk)
 import matplotlib.pyplot as pplt
 import portreader
+import vlc
 import altitudemeasurement
 
 #declaring root window
 root = Tk()
 
+#declaring needed variables
+player = vlc.MediaPlayer("alarm.mp3")
 altitude = 0
 y = []
 temperatures = []
 pascals = []
 
+#function for displaying rain warning popup
 def precipitation_warning ():
-    messagebox.showerror("PRECIPITATION WARNING!", "RAIN WARNING!") 
+    player.play()
+    messagebox.showerror("PRECIPITATION WARNING!", "RAIN WARNING!")
+    player.stop()
 
-#initializing window
+#initializing window/root
 root.title("Weather App") #window title
 root.configure(background="grey")
 root.minsize(800, 800)  # width, height
 root.maxsize(1920, 1080)  # setting (arbitrary) max size
 root.geometry("600x600+150+150")  # width x height + x + y
 
-#label1 = ttk.Label(main_window, text="Temperature Data: ").grid(column=0, row=0, sticky=W)
-
+#Get's measurement data from portreader and separates temperature measurements
+#from air pressure measurements and returns a 'touple' of lists (temp, ap)
 def get_measurements():
     y = portreader.get_data()
 
@@ -48,6 +54,8 @@ def get_measurements():
         i+=1
     return [temperatures, pascals]
 
+#Triggered on click of temperature_button. Displays the measurement data
+#in two matplotlib plots.
 def temperature_button_click():
    # Plot figure
     fig = Figure(figsize = (5, 5), dpi = 100)
@@ -76,6 +84,7 @@ def temperature_button_click():
     # placing the toolbar on the Tkinter window
     canvas.get_tk_widget().place(x=50,y=50)
 
+    #plotting the second graph, i.e air pressure graph.
     pplt.plot(x, pascals)
     pplt.xlabel("Time: ")
     pplt.ylabel("Air pressure in pascals ")
@@ -83,6 +92,8 @@ def temperature_button_click():
     pplt.show()
 
 
+#Get's user-input and finds altitude for that specific latitude and logitude.
+#field1 = lat, field2=long. Returns altitude (as string)
 def get_alt():
     altitude = altitudemeasurement.get_altitude(entry_field1.get(), entry_field2.get())
     messagebox.showwarning("Altitude at lat, long", altitude) 
@@ -90,17 +101,23 @@ def get_alt():
 
 #creating button to call graph
 temperature_button = Button(root, activebackground="dark gray", bd=4, command=temperature_button_click, width=20, height=5, text="Display temperature data")
+
+#Initialize entry fields for lat/long user input
 entry_field1 = Entry(root, textvariable="Latitude: ")
 entry_field2 = Entry(root, textvariable="Longitude: ")
+
+#initialize button for altitude calculation trigger
 height_button = Button(root, activebackground="dark gray", bd=4, 
                             command=get_alt, 
                             width=20, height=5, text="Get height at location")
 
+#Placing the different widgets in the window
 entry_field1.place(x=400, y=150)
 entry_field2.place(x=400, y =400)
 temperature_button.place(x=150, y=150)
 height_button.place(x=150, y=400)
 
+#displays the window
 root.mainloop()
 
 precipitation_warning()
